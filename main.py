3a9eb088744231111f0d4290e8b017744d0a8b63
zidane64 <z32220252@gmail.com>
2026-05-18 09:37:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -112,7 +112,3 @@ def dashboard():
     </html>
     """
 
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
